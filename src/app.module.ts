923a70422f8d0845aa4a123ddda39fd3547f16f6
Thanathorn1 <thanatornfever@gmail.com>
2026-02-20 15:45:20 +0700

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,60 @@
+import { Module } from '@nestjs/common';
+
+
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { RiderModule } from './users/rider/rider.module';
+
+import { APP_GUARD } from '@nestjs/core';
+
+
+@Module({
+
+  imports: [ConfigModule.forRoot({
+
+    isGlobal: true,
+
+  }),
+  // ตั้งค่า rate limiting โดยใช้ ThrottlerModule  
+
+  ThrottlerModule.forRoot([
+
+    {
+
+      ttl: 60_000,  // 1 minute 
+
+      limit: 100,   // 100 requests per minute 
+
+    },
+
+  ]),
+  MongooseModule.forRootAsync({
+
+    imports: [ConfigModule],
+
+    inject: [ConfigService],
+
+    useFactory: (configService: ConfigService) => ({
+
+      uri: configService.get<string>('MONGO_URI'),
+
+    }),
+
+  }), UsersModule, AuthModule, RiderModule],
+
+
+
+
+
+  // *** สำหรับการตั้งค่า global guard กรณีกันโดนยิง API รัว ๆ ทั้งระบบ ThrottlerGuard *** 
+
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
+
+})
+
+export class AppModule { } 
